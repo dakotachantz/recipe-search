dab06d8b41bf0cf967f8b463ec765f016dca61ae
Dakota Nichols <dakotachantz@gmail.com>
2017-08-08 13:00:56 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+// on enter or on click fa search the term
+// onclick box go to link of recipe
+// onhover box reveal list of ingredients
+// on load display title of recipe
+// on load display img of recipe as background of box
+
+// if results[i].thumbnail === "" then {background img of box = }
+
+let box = `
+<div class="box">
+    <p>Title of Recipe</p>
+</div>
+`
+
+document.querySelector(".row").innerHTML = box;
+
